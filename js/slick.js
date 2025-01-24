30b93496fc4c58aafcbfe86536f8392d625fbd60
olesyamaniuk <maniukolesya222@gmail.com>
2025-01-24 14:48:12 +0200

    2

--- js/slick.js
+++ js/slick.js
@@ -9,7 +9,7 @@ $(function () {
     autoplay: true,
     autoplaySpeed: 3500,
     speed: 1000,
-    dots: false,
+    dots: true,
     centerMode: true,
     cssEase: 'linear',
     prevArrow: '.gallery-list-left',
@@ -35,7 +35,22 @@ $(function () {
   });
 });
 
-
+$(function () {
+  $('.faq-container').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    variableWidth: true,
+    autoplay: true,
+    autoplaySpeed: 3500,
+    speed: 1000,
+    dots: true,
+    centerMode: true,
+    cssEase: 'linear',
+    prevArrow: '.reviews-list-left',
+    nextArrow: '.reviews-list-right',
+  });
+});
 
 
 
